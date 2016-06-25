bf1c93abaa51bfdc248766d8aa4cf5714812fb6d
Rainer Friederich <rainer@wondrous.ch>
2016-06-25 09:57:46 +0200

    Added first Install / Uninstall Test

---
+++ tests/src/Functional/ParagraphsTypesInstallUninstallTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\Tests\paragraphs_types\Functional;
+
+use Drupal\FunctionalJavascriptTests\JavascriptTestBase;
+
+class ParagraphsTypesInstallUninstallTest extends JavascriptTestBase {
+
+  public static $modules = array('paragraphs_types');
+
+  public function setUp() {
+    parent::setUp();
+
+    $admin_user = $this->drupalCreateUser(array(
+      'administer paragraphs types',
+      'administer modules',
+    ));
+    $this->drupalLogin($admin_user);
+  }
+
+  public function testUninstall() {
+
+    $session = $this->getSession();
+
+    // Delete Paragraphs data.
+    $this->drupalGet('admin/structure/paragraphs_type/uninstall');
+    $this->assertSession()->pageTextContains(t('Prepare'));
+    $this->submitForm([], t('Delete Paragraphs data'));
+    $this->waitForAjaxToFinish();
+    $html = $session->getPage()->getHtml();
+    $this->assertContains('Paragraphs data has been deleted.', $html);
+
+    // Uninstall the module.
+    $this->drupalGet('admin/modules/uninstall');
+    $this->submitForm(['uninstall[paragraphs_types]' => TRUE], t('Uninstall'));
+    $this->submitForm([], t('Uninstall'));
+    $html = $session->getPage()->getHtml();
+    $this->assertContains('The selected modules have been uninstalled.', $html);
+    $this->assertContains('Paragraphs', $html);
+    $this->assertNotContains('Paragraphs Types', $html);
+  }
+
+  /**
+   * Waits for jQuery to become active and animations to complete.
+   */
+  protected function waitForAjaxToFinish() {
+    $condition = "(0 === jQuery.active && 0 === jQuery(':animated').length)";
+    $this->assertJsCondition($condition, 10000);
+  }
+}
