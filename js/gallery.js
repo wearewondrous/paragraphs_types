a961710d058462be4b62aaa9453e5e7018819a62
Rainer Friederich <rainer@wondrous.ch>
2016-08-22 17:03:00 +0200

    change inline javascript to drupal settings

--- js/gallery.js
+++ js/gallery.js
@@ -1,10 +1,10 @@
-jQuery(document).ready(function($) {
+(function($, Drupal, drupalSettings) {
   'use strict';
 
   var openPhotoSwipe = function(galleryIndex, imgIndex, animationOrigin) {
     var pswpElement = document.querySelectorAll('.pswp')[0];
     // define the gallery
-    var items = paragraphGalleries[galleryIndex];
+    var items = drupalSettings['ps_images'][galleryIndex];
     // define options (if needed)
     var options = {
       getThumbBoundsFn: function() {
@@ -17,18 +17,19 @@ jQuery(document).ready(function($) {
       showAnimationDuration: 333,
       hideAnimationDuration: 333,
       shareEl: false,
-      zoomEl: false,
+      zoomEl: false
     };
     var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
     gallery.listen('gettingData', function(index, item) {
       if (item.w < 1 || item.h < 1) { // unknown size
         var img = new Image();
         img.onload = function() { // will get size after load
+
           item.w = this.width; // set image width
           item.h = this.height; // set image height
           gallery.invalidateCurrItems(); // reinit Items
           gallery.updateSize(true); // reinit Items
-        }
+        };
         img.src = item.src; // let's download image
       }
     });
@@ -40,7 +41,7 @@ jQuery(document).ready(function($) {
     if ($(this).hasClass('img-thumbnail')) {
       imgIndex = $('.img-thumbnail').index($(this));
     }
-    var galleryIndex = 0;
+    var galleryIndex = $(this).data('paragraph-id');
     openPhotoSwipe(galleryIndex, imgIndex, null);
   });
-});
+})(jQuery, Drupal, drupalSettings);
